d0b257ffa4b22cd4a1d00daee0c23b1517a9f5e6
Natalie Tang <108561054+natalientang@users.noreply.github.com>
2023-07-22 21:26:17 -0500

    created dtos (#1)

---
+++ adoptAPetopia/src/main/java/com/we/adoptAPetopia/entities/Adopter.java
@@ -0,0 +1,75 @@
+package com.we.adoptAPetopia.entities;
+
+import java.util.Objects;
+
+public class Adopter {
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Adopter adopter = (Adopter) o;
+
+        if (id != adopter.id) return false;
+        if (!Objects.equals(name, adopter.name)) return false;
+        if (!Objects.equals(email, adopter.email)) return false;
+        if (!Objects.equals(phone, adopter.phone)) return false;
+        return Objects.equals(address, adopter.address);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (phone != null ? phone.hashCode() : 0);
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        return result;
+    }
+}
---
+++ adoptAPetopia/src/main/java/com/we/adoptAPetopia/entities/Species.java
@@ -0,0 +1,53 @@
+package com.we.adoptAPetopia.entities;
+
+import java.util.Objects;
+
+public class Species {
+    private int id;
+    private String name;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Species species = (Species) o;
+
+        if (id != species.id) return false;
+        if (!Objects.equals(name, species.name)) return false;
+        return Objects.equals(description, species.description);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+}

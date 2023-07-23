3692496abcee6282e1cf77ed0fc9063ae1b62942
Natalie Tang <natalietang77@gmail.com>
2023-07-22 21:00:39 -0500

    add initial project

---
+++ adoptAPetopia/src/main/java/com/we/adoptAPetopia/AdoptAPetopiaApplication.java
@@ -0,0 +1,13 @@
+package com.we.adoptAPetopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptAPetopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptAPetopiaApplication.class, args);
+	}
+
+}
---
+++ adoptAPetopia/src/test/java/com/we/adoptAPetopia/AdoptAPetopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.we.adoptAPetopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptAPetopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

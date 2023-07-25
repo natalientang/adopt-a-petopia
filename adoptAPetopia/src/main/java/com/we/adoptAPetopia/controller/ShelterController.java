91e90b399ec776eae3deee9db0f6c86c6a12d9be
Natalie Tang <natalietang77@gmail.com>
2023-07-24 23:57:05 -0500

    completed species controller

--- adoptAPetopia/src/main/java/com/we/adoptAPetopia/controller/ShelterController.java
+++ adoptAPetopia/src/main/java/com/we/adoptAPetopia/controller/ShelterController.java
@@ -9,7 +9,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.naming.Binding;
 import javax.validation.Valid;
 import java.util.List;
 
--- adoptAPetopia/src/main/java/com/we/adoptAPetopia/controller/SpeciesController.java
+++ adoptAPetopia/src/main/java/com/we/adoptAPetopia/controller/SpeciesController.java
@@ -1,4 +1,71 @@
 package com.we.adoptAPetopia.controller;
 
+import com.we.adoptAPetopia.entities.Species;
+import com.we.adoptAPetopia.service.SpeciesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
 public class SpeciesController {
+    @Autowired
+    SpeciesService speciesService;
+
+    @GetMapping("species")
+    public String displaySpecies(Model model) {
+        List<Species> species = speciesService.getAllSpecies();
+        model.addAttribute("species", species);
+        return "species";
+    }
+
+    @GetMapping("addSpecies")
+    public String displayAddSpecies(Model model) {
+        Species species = new Species();
+        model.addAttribute("species", species);
+        return "addSpecies";
+    }
+
+    @PostMapping("addSpecies")
+    public String addSpecies (@Valid Species species, BindingResult result) {
+        if (result.hasErrors()) {
+            return "addSpecies";
+        }
+        speciesService.addSpecies(species);
+        return "redirect:/species";
+    }
+
+    @GetMapping("editSpecies")
+    public String displayEditSpecies(Integer id, Model model) {
+        Species species = speciesService.getSpeciesById(id);
+        model.addAttribute("species", species);
+        return "editSpecies";
+    }
+
+    @PostMapping("editSpecies")
+    public String editSpecies(@Valid Species species, BindingResult result) {
+        if (result.hasErrors()) {
+            return "editSpecies";
+        }
+        speciesService.updateSpecies(species);
+        return "redirect:/species";
+    }
+
+    @GetMapping("deleteSpecies")
+    public String deleteSpecies(Integer id) {
+        speciesService.deleteSpeciesById(id);
+        return "redirect:/species";
+    }
+
+    @GetMapping("speciesDetail")
+    public String speciesDetail(Integer id, Model model) {
+        Species species = speciesService.getSpeciesById(id);
+        model.addAttribute("species", species);
+        return "speciesDetail";
+    }
 }
